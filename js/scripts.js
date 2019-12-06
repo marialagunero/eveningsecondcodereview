58f4ae3338c211ecf59fcb9e9fa39968dba7c8a3
Maria Lagunero <maria58@gmail.com>
2019-12-06 12:45:40 -0800

    add functions and if statements to scripts. Still working on broken code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,31 @@
 $(document).ready(function(){
   $(form#language).submit(function(event){
     event.preventDefault();
-    var colorChoice = parseInt($("#colorChoice").val())
+    var animalChoice = parseInt($("#animalChoice").val());
+    var colorChoice = parseInt($("#colorChoice")).val();
+    var hobbyChoice = parseInt($("#hobbyChoice")).val();
+    var result = (colorChoice + foodChoice + hobbyChoice);
+
+    if (result <=3) {
+      $("#Ruby").show();
+      $("#Python,#Csharp").hide();
+
+    } else if (result <=6) {
+      $("#Python").show();
+      $("#Ruby, #Csharp").hide();
+
+    } else if (result <=9) {
+      $("#Csharp").show();
+      $("#Ruby, #Python").hide();
 
 
-  })
+      return (result)
+
+    };
 
 
 
 
+  });
 
 });
