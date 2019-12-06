00ff7e28b435027f01cace8a9b22456b97e77d30
Maria Lagunero <maria58@gmail.com>
2019-12-06 13:01:53 -0800

    add changes to scripts variable names and sytax errors. Code is now working.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
-$(document).ready(function(){
-  $(form#language).submit(function(event){
+$(document).ready(function() {
+  $("form#language").submit(function(event) {
     event.preventDefault();
     var animalChoice = parseInt($("#animalChoice").val());
-    var colorChoice = parseInt($("#colorChoice")).val();
-    var hobbyChoice = parseInt($("#hobbyChoice")).val();
-    var result = (colorChoice + foodChoice + hobbyChoice);
+    var colorChoice = parseInt($("#colorChoice").val());
+    var hobbyChoice = parseInt($("#hobbyChoice").val());
+    var result = (animalChoice + colorChoice + hobbyChoice)
 
-    if (result <=3) {
+    if (result <= 3) {
       $("#Ruby").show();
       $("#Python,#Csharp").hide();
 
-    } else if (result <=6) {
+    } else if (result <= 6) {
       $("#Python").show();
       $("#Ruby, #Csharp").hide();
 
-    } else if (result <=9) {
+    } else if (result <= 9) {
       $("#Csharp").show();
       $("#Ruby, #Python").hide();
 
